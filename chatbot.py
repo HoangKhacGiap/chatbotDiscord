c8bf433f4e28dcb538b0294cb9d857ba4fbdd26c
giapgiap <hoangkhacgiap1108@gmail.com>
2024-08-17 22:33:10 +0700

    test multiple message

--- chatbot.py
+++ chatbot.py
@@ -7,29 +7,43 @@ import time
 # token = "input your token here"
 
 print ("=====================auto chat bot==========================")
-channelId = input("|input your channel id here: ")
 token = input("|input your token here: ")
+channelId = input("|input your channel id here: ")
 
 url = "https://discord.com/api/v9/channels/" + channelId + "/messages"
 headers = {
     "Authorization" : token
 }
 
-def send_message(random_time):
+def send_message(random_time, message):
     payload = {
-        "content": f"test owo chatbot in {random_time} seconds"
+        "content": f"{message}"
     }
     res = requests.post(url, json=payload, headers=headers)
     print(f"Message sent: {res.status_code} + time: {random_time} ")
 
-interval_min = 1  # Minimum interval in seconds
-interval_max = 10  # Maximum interval in seconds
-    
+interval_min = 15  # Minimum interval in seconds
+interval_max = 16  # Maximum interval in seconds
+
+
 # Get the number of iterations from the user
-num_iterations = int(input("|Số lần test: "))
+# num_iterations = int(input("|Số lần test: "))
+
+# Get the list of messages from the user
+messages = []
+# for i in range(num_iterations):
+while True:
+    i=0
+    message = input(f"|Nhập tin nhắn thứ {i+1}: ")
+    if message.upper() == "OK":
+        break
+    messages.append(message)  
 print("============================================================")
 
-for _ in range(num_iterations):
-    interval = random.randint(interval_min, interval_max)
-    send_message(interval)
-    time.sleep(interval)
+# Send the messages
+# for _ in range(num_iterations):
+while True:
+    for message in messages:
+        interval = random.randint(interval_min, interval_max)
+        send_message(interval, message)
+        time.sleep(interval)
